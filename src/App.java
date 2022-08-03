08a3308f357b2559f69899c94df4309dcff4fe93
ijm <ijm00@hotmail.com>
2022-08-03 00:09:33 -0300

    problema de 8 threads rodando ao mesmo tempo resolvido

--- src/App.java
+++ src/App.java
@@ -1,62 +1,58 @@
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import caminhao.Caminhao;
 import caminhao.FilaDeCaminhoes;
 
 public class App {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ConcurrentLinkedQueue<Caminhao> fila = FilaDeCaminhoes.getInstance().getFila();
+
         Azeitona azeitonaGalega = new Azeitona("Galega");
         Azeitona azeitonaCordovil = new Azeitona("Cordovil");
         Azeitona azeitonaPicual = new Azeitona("Picual");
 
-        Plantacao plantacao1 = new Plantacao(azeitonaGalega, 4);
-        Plantacao plantacao2 = new Plantacao(azeitonaGalega, 4);
-        Plantacao plantacao3 = new Plantacao(azeitonaCordovil, 3);
-        Plantacao plantacao4 = new Plantacao(azeitonaCordovil, 3);
-        Plantacao plantacao5 = new Plantacao(azeitonaPicual, 2);
-
-        ExecutorService produzir = Executors.newFixedThreadPool(5);
-        ConcurrentLinkedQueue<Caminhao> fila = FilaDeCaminhoes.getInstance().getFila();
-
-        try {
-            produzir.execute(plantacao1.produzirTask(2));
-            produzir.execute(plantacao2.produzirTask(2));
-            produzir.execute(plantacao3.produzirTask(2));
-            produzir.execute(plantacao4.produzirTask(2));
-            produzir.execute(plantacao5.produzirTask(2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            produzir.shutdown();
-        }
-
-        produzir.shutdown();
-
+        List<Plantacao> plantacoes = new ArrayList<>() {
+            {
+                add(new Plantacao(azeitonaGalega, 4));
+                add(new Plantacao(azeitonaGalega, 4));
+                add(new Plantacao(azeitonaCordovil, 3));
+                add(new Plantacao(azeitonaCordovil, 3));
+                add(new Plantacao(azeitonaPicual, 2));
+            }
+        };
 
         Lagar lagar = new Lagar(3);
         RecepcaoLagar recepcao = new RecepcaoLagar();
 
         ExecutorService descarregarCaminhoes = Executors.newFixedThreadPool(lagar.getNumeroPortasRecepcao());
-        try {
-            while (!produzir.isTerminated()) {
-                if (!FilaDeCaminhoes.getInstance().getFila().isEmpty())
-                    descarregarCaminhoes.execute(recepcao.descarregarCaminhoesTask());
 
+        final var inicioOperacao = LocalDateTime.now();
+
+        System.out.println("Iniciando execução...");
+
+        while (LocalDateTime.now().isBefore(inicioOperacao.plusMinutes(2))) {
+
+            for (Plantacao plantacao : plantacoes) {
+                plantacao.produzir();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            descarregarCaminhoes.shutdown();
+
+            descarregarCaminhoes.execute(recepcao.descarregarCaminhoesTask());
+
         }
+        descarregarCaminhoes.shutdown();
 
         ExecutorService descarregarRestantes = Executors.newFixedThreadPool(lagar.getNumeroPortasRecepcao());
         try {
             while (true) {
-                if (produzir.isTerminated() && descarregarCaminhoes.isTerminated()) {
+                if (descarregarCaminhoes.isTerminated()) {
                     // System.out.println("Aguardando caminhoes remanescentes");
                     Thread.sleep(15_000);
                     while (!fila.isEmpty()) {
--- src/Plantacao.java
+++ src/Plantacao.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -48,75 +49,74 @@ public class Plantacao {
         return distanciaAteLagar;
     }
 
-    public Runnable produzirTask(Integer limiteTempoProducaoMinutos) {
+    public void produzir() {
+         try {
+            ExecutorService produzir = Executors.newSingleThreadExecutor();
+             produzir.submit(produzirTask());
+             produzir.shutdown();
+             produzir.awaitTermination(this.getDistanciaAteLagar() + 8, TimeUnit.SECONDS);// TODO 8 = maximo tempo
+                                                                                          // possivel carregamento >>
+                                                                                          // inserir variavel
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Runnable produzirTask() {
         return () -> {
-            final LocalDateTime inicioOperacao = LocalDateTime.now();
-           
-            while (LocalDateTime.now().isBefore(inicioOperacao.plusMinutes(limiteTempoProducaoMinutos))) {
-                
-                if (this.produzindo) {
-                    this.abastecerCaminhao().despacharCaminhao();
-                }
 
-                if (fila.get().size() >= 12) {
-                    try {
-                        this.suspenderProducao();
-                        System.out.println("Produção suspensa!" + " O tamanho da fila é " + fila.get().size() + ".");
-                        Thread.sleep(500);
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if (!this.produzindo && fila.get().size() <= 4) {
-                    this.retomarProducao();
-                    System.out.println("Plantação " + this.codigo + " retomando produção!");
-                }
+            if (this.produzindo && !this.abastecendoCaminhao) {
+                this.abastecerCaminhao();
+                this.despacharCaminhao();
             }
 
-                
+            if (fila.get().size() >= 12) {
+                try {
+                    this.suspenderProducao();
+                    System.out.println("Produção suspensa!" + " O tamanho da fila é " + fila.get().size() + ".");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            if (!this.produzindo && fila.get().size() <= 4) {
+                this.retomarProducao();
+                System.out.println("Plantação " + this.codigo + " retomando produção!");
+            }
+
         };
     }
 
-    
     public void suspenderProducao() {
-        pauseLock.lock();
         this.produzindo = false;
-
-        
     }
 
     public void retomarProducao() {
         this.produzindo = true;
-        unpaused.signalAll();
-      
-        pauseLock.unlock();
-      
     }
 
     public boolean isProduzindo() {
         return this.produzindo;
     }
 
-    public Plantacao abastecerCaminhao() {
-        if (this.produzindo && !this.abastecendoCaminhao) {
+    public void abastecerCaminhao() {
+
+        try {
+            this.abastecendoCaminhao = true;
             this.caminhao = this.requisitarCaminhao();
             this.caminhao.getRelatorio().setCodigoPlantacao(this.codigo);
             this.caminhao.getRelatorio().setTipoAzeitona(this.getAzeitona().getVariedade());
             // System.out.println("Abastecendo caminhão " + this.caminhao);
-            abastecendoCaminhao = true;
-            try {
-                Thread.sleep(this.caminhao.getTempoProcessamentoMillis());
-                this.caminhao.avancaEstado();
-                this.abastecendoCaminhao = false;
-                // System.out.println(this.caminhao + " " + this.caminhao.getEstado());
+            Thread.sleep(this.caminhao.getTempoProcessamentoMillis());
+            this.caminhao.avancaEstado();
+            this.abastecendoCaminhao = false;
+            // System.out.println(this.caminhao + " " + this.caminhao.getEstado());
 
-            } catch (InterruptedException ie) {
-                ie.printStackTrace();
-            }
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
-        return this;
+
     }
 
     private Caminhao requisitarCaminhao() {
